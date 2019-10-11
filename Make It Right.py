348f34b6a3e52a6b43adee005f455101aca82e29
AliRn76 <alirn76@yahoo.com>
2019-10-12 01:06:21 +0330

    Ye kari kardam tooye folder ro ham dorost kone ,VALI KAR NAKARD

--- Make It Right.py
+++ Make It Right.py
@@ -1,27 +1,28 @@
 # Created By AliRn.ir
 
 import os
+for dirpath, dirnames, files in os.walk('.'):
+    print(f'Found directory: {dirpath}')
+    for file in files:
+        if file.endswith('.srt'):
+            base = os.path.splitext(file)[0]
+            os.rename(file, base + ".txt")
+            fileName = base + '.txt'
 
-for file in os.listdir("."):
-    if file.endswith('.srt'):
-        base = os.path.splitext(file)[0]
-        os.rename(file, base + ".txt")
-        fileName = base + '.txt'
+            try:
+                with open(fileName, 'r', encoding="utf-8") as output:
+                    data = output.read()
 
-        try:
-            with open(fileName, 'r', encoding="utf-8") as output:
-                data = output.read()
+                os.rename(fileName, base + ".srt")
 
-            os.rename(fileName, base + ".srt")
+                if data:
+                    continue
 
-            if data:
-                continue
+            except:
+                with open(fileName, 'r', encoding="cp1256") as output2:
+                    data2 = output2.read()
 
-        except:
-            with open(fileName, 'r', encoding="cp1256") as output2:
-                data2 = output2.read()
+                with open(fileName, 'w', encoding="utf-8") as input1:
+                    input1.write(data2)
 
-            with open(fileName, 'w', encoding="utf-8") as input1:
-                input1.write(data2)
-
-            os.rename(fileName, base + ".srt")
+                os.rename(fileName, base + ".srt")
